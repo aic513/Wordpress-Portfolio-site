62f5aa458166782ea89c9222b3688c2e03d5c5da
aic513 <aic513@mail.ru>
2017-02-26 22:22:36 +0300

    Добавил слайдер в запись портфолио

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,23 +1,33 @@
 <div class="footer-wrapper">
-	<div class="footer-main">
-    	<div class="foter-left">
+    <div class="footer-main">
+        <div class="foter-left">
             <a href="/"><img src="<?php bloginfo('template_url') ?>/images/logo-ftr.jpg" alt="" /></a>
             <p>copyright 2011 <a href="#">www.yoururl.com</a></p>
         </div>
         <div class="foter-right">
-	        <?php if(!dynamic_sidebar( 'footer' )): ?>
-			        <p style="color:white">Место для футера (добавить через текст)</p>
-	        <?php endif; ?>
+            <?php if(!dynamic_sidebar( 'footer' )): ?>
+                <p style="color: #FEFCFC">Добавьте содержимое через виджет Текст</p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquerypp.custom.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.elastislide.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.liSlidik.js"></script>
 <script type="text/javascript">
-		
-	$( '#carousel' ).elastislide();
-			
+
+    $( '#carousel' ).elastislide();
+
+    $(function(){
+        $(window).load(function(){
+            $('#slide_2').liSlidik({
+                auto:3000,
+                duration: 1000
+            });
+        })
+    });
+
 </script>
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -28,3 +28,34 @@ register_sidebar(array(
 	'description' => 'Добавьте ссылки на соцсети через виджет Текст',
 	'before_widget' => '',
 	'after_widget' => ''));
+
+/*
+ * shortcode галлереи
+ */
+
+function portfolio_gallery($attr, $text=''){
+	// получаем массив ID картинок
+	$img_src = explode(",", $attr['ids']);
+	// шаблон удаления атрибутов width/height
+	$pattern = '#(width|height)="\d+"#';
+	$return = '<ul id="slide_2" class="slidik">';
+	// счетчик
+	$i = 1;
+	foreach ($img_src as $item) {
+		// получаем HTML-код картинки
+		$image_url = wp_get_attachment_image( $item, 'full' );
+		// вырезаем атрибуты width/height
+		$image_url = preg_replace($pattern, "", $image_url);
+		// формируем вывод картинок
+		if($i == 1) $return .= '<li class="show">' . $image_url . '</li>';
+		else $return .= '<li>' . $image_url . '</li>';
+		$i++;
+	}
+	$return .= '<a data-slidik="slide_2" class="next" href="#">Next</a>
+            <a data-slidik="slide_2" class="prev" href="#">Prev</a>
+            <div class="captionWrap"><div data-slidik="slide_2" class="caption"></div></div>
+            <div class="portfolio-close"><a href="#"></a></div>
+    	</ul>';
+	echo $return;
+}
+add_shortcode('shortcode_gallery', 'portfolio_gallery');
