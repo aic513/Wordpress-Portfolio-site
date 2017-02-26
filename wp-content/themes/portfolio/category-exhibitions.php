fdcfecdb7e90c41318f722e3dffe461cdae4e4e0
aic513 <aic513@mail.ru>
2017-02-26 22:40:02 +0300

    Добавил вывод метаданных в рубрики и выставки

--- wp-content/themes/portfolio/category-exhibitions.php
+++ wp-content/themes/portfolio/category-exhibitions.php
@@ -1,32 +1,41 @@
 <?php get_header(); ?>
-<div class="content-main">
+    <div class="content-main">
 
 
-	<div class="content-main-blocks">
+        <div class="content-main-blocks">
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div>
-	<a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a>
-    <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
-    <p class="ex-place">King’s Museum, Kensington, London</p>
-	<p class="ex-date">Tues 26th April 2011 - Sat 30th April 2011
-8am to 9pm with free refreshments.</p>
-	<p class="ex-ticket">Buy tickets from <a href="#">TicketMaster</a></p>
-    <?php the_excerpt() ?>
-    <p><a href="<?php the_permalink() ?>" class="read-more"> Read more</a></p>
-</div>
+                <div>
+                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a>
+                    <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
 
-<?php endwhile; ?>
-<!-- post navigation -->
-<?php else: ?>
-<!-- no posts found -->
-<?php endif; ?>
+                    <?php $custom_fields = get_post_custom(get_the_ID()); ?>
+                    <!-- place, date, ticket -->
+                    <?php if ($custom_fields['place'][0]): ?>
+                        <p class="ex-place"><?php echo $custom_fields['place'][0] ?></p>
+                    <?php endif; ?>
+                    <?php if ($custom_fields['date'][0]): ?>
+                        <p class="ex-date"><?php echo $custom_fields['date'][0] ?></p>
+                    <?php endif; ?>
+                    <?php if ($custom_fields['ticket'][0]): ?>
+                        <p class="ex-ticket"><?php echo $custom_fields['ticket'][0] ?></p>
+                    <?php endif; ?>
+                    <!-- place, date, ticket -->
 
+                    <?php the_excerpt() ?>
+                    <p><a href="<?php the_permalink() ?>" class="read-more"> Read more</a></p>
+                </div>
+
+            <?php endwhile; ?>
+                <!-- post navigation -->
+            <?php else: ?>
+                <!-- no posts found -->
+            <?php endif; ?>
+
+
+        </div>
 
 
     </div>
-    
-            
-</div>
 <?php get_footer(); ?>
